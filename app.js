88e3da34fed59ed613054e521b2c5ef0fee536b3
wyy <wuyanyi09@gmail.com>
2014-02-11 22:35:10 +0800

    ls

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 var express = require("express");
-var logger = require("log4js").getLogger();
+var logger = require("log4js").getLogger("app.js");
 var fs = require("fs"); 
+var http = require("http");
 var app = express();
 
+app.use(express.bodyParser());
+
 app.get('/cppjieba', function(req, res){
-    //var body = "hello world";
-    //logger.debug(body);
     fs.readFile("./public/index.html", function(err, data){
         if(err){
             return err;
         }
         res.set("Content-Type", "text/html;charset=utf-8");
         res.send(data);
-        //logger.debug(data);
+    });
+});
+
+app.post('/', function(req, res) {
+    var options = {
+        host : 'localhost',
+        port : '11200',
+        path : '/?key=' + req.body.sentence,
+        method : 'GET'
+    };
+    console.log(req.body.sentence);
+    //res.send(req.body.sentence + req.body.sentence);
+    var reqGet = http.request(options, function(resGet){
+        resGet.on('data', function(d){
+            res.send(d);
+        });
+    });
+    reqGet.end();
+    reqGet.on('error', function(err){
+        console.error(err);
     });
 });
 

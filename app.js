29cb974c18540b93e1351eb49acc66c34369f647
wyy <wuyanyi09@gmail.com>
2014-02-12 21:40:41 -0800

    ci

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var logger = require("log4js").getLogger("app.js");
-//var fs = require("fs"); 
+var fs = require("fs"); 
 var http = require("http");
 var nodejieba = require("nodejieba");
 var app = express();
@@ -11,13 +11,13 @@ app.use(express.bodyParser());
 
 app.get('/cppjieba', function(req, res){
     res.send("hhehe");
-    //fs.readFile("./public/index.html", function(err, data){
-    //    if(err){
-    //        return err;
-    //    }
-    //    res.set("Content-Type", "text/html;charset=utf-8");
-    //    res.send(data);
-    //});
+    fs.readFile("./public/index.html", function(err, data){
+        if(err){
+            return err;
+        }
+        res.set("Content-Type", "text/html;charset=utf-8");
+        res.send(data);
+    });
 });
 
 app.post('/', function(req, res) {
@@ -32,9 +32,7 @@ app.post('/', function(req, res) {
     console.log("%s -> %s", req.body.sentence, words);
 });
 
-var port = 5000;
-
-app.listen(port);
+var port = (process.env.PORT || 5000);
 
 logger.info("Listening on port %d", port);
 

b134c000ad3c0a2cf948fc82630fc48be55f3f42
wyy <wuyanyi09@gmail.com>
2014-02-12 21:09:25 -0800

    ci

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var logger = require("log4js").getLogger("app.js");
-var fs = require("fs"); 
+//var fs = require("fs"); 
 var http = require("http");
 var nodejieba = require("nodejieba");
 var app = express();
@@ -10,13 +10,14 @@ nodejieba.loadDict("./node_modules/nodejieba/dict/jieba.dict.utf8", "./node_modu
 app.use(express.bodyParser());
 
 app.get('/cppjieba', function(req, res){
-    fs.readFile("./public/index.html", function(err, data){
-        if(err){
-            return err;
-        }
-        res.set("Content-Type", "text/html;charset=utf-8");
-        res.send(data);
-    });
+    res.send("hhehe");
+    //fs.readFile("./public/index.html", function(err, data){
+    //    if(err){
+    //        return err;
+    //    }
+    //    res.set("Content-Type", "text/html;charset=utf-8");
+    //    res.send(data);
+    //});
 });
 
 app.post('/', function(req, res) {
